b00d2bd3753bfe811b4573dd657b374dae50c6dc
melo <melo@163.com>
2017-10-27 13:20:42 +0800

    Signed-off-by: melo <melo@163.com>

---
+++ js/sp209-1.8.1.js
@@ -0,0 +1,144 @@
+/*
+ *除非自己行 靠谁都不行
+ *come on ！！！！！！！！！ 
+ *1、tab
+ */
+(function($){
+	var parameter={
+		effect:"left",
+		current:1,
+		eventWays:"click"
+	}
+
+	function tab(self,opts){
+		
+		this.opts=jQuery.extend({},parameter,opts);
+		this.btn = self.find('ul.tab-btn>li');
+		this.btnBox = self.find('ul.tab-btn');
+		this.txt = self.find('ul.tab-txt>li');
+		this.txtBox = self.find('ul.tab-txt');
+		this.length=self.find('ul.tab-btn>li').length;
+		//this.current=1; // 控制当前显示的是哪一个
+
+		this.gdW=this.txtBox.parent().width(); //获取slide的宽度
+
+		console.log(this.gdW);
+
+
+
+		this.init(); //tab 对象调用原型方法
+
+		
+	}
+
+	tab.prototype={
+		init:function(){
+			var self = this;
+			self.layout();
+
+			switch (this.opts.effect){
+				case "hide":
+					if (this.opts.eventWays=='click') {
+						self.btn.click(function() {
+							var index =$(this).index();
+							self.effect_default(index);
+						})
+					}else{
+						self.btn.hover(function() {
+							var index =$(this).index()
+							self.effect_default(index);
+						})
+					}
+					break;
+				
+				case "fade":
+					if (this.opts.eventWays=='click') {
+						self.btn.click(function() {
+							var index =$(this).index()
+							self.effect_fade(index);
+						})
+					}else{
+						self.btn.hover(function() {
+							var index =$(this).index();
+							t=setTimeout(function(){
+								self.effect_fade(index);
+							},180)
+						},function(){
+							clearTimeout(t);
+						})
+					}
+					break;
+
+				case "left":
+					if (this.opts.eventWays=='click') {
+						self.btn.click(function() {
+							var index =$(this).index();
+							self.num=index;
+							self.effect_left(index);
+						})
+					}else{
+						self.btn.hover(function() {
+							var index =$(this).index();
+							self.num=index;
+							self.effect_left(index);
+						})
+					}
+					break;
+
+				default:
+					self.btn.click(function() {
+						var index =$(this).index()
+						self.effect_default(index);
+					})
+			}
+		},
+		effect_default:function(index){
+			if (index || index==0) {
+				this.opts.current=index
+			}else{
+				// 没有就没有 傻吊啊
+			}
+			this.txt.hide().eq(this.opts.current).show();
+			this.btn.eq(this.opts.current).addClass('on').siblings().removeClass('on');
+		},
+		effect_fade:function(index){
+			if (index || index==0) {
+				this.opts.current=index
+			}else{
+				// 没有就没有 傻吊啊
+			}
+			this.btn.eq(index).addClass('on').siblings().removeClass('on');
+			this.txt.hide(0).eq(index).fadeIn(200);
+			
+		},
+		effect_left:function(index){
+			this.btn.eq(index).addClass('on').siblings().removeClass('on');
+			this.txtBox.stop(true,true).animate({marginLeft: -this.gdW*index},300, function() {});
+		},
+		layout:function(){
+			if (this.opts.effect=="left") {
+				this.txtBox.css({
+					overflow: 'hidden',
+					width: this.length * this.gdW
+				});
+				this.txt.css({
+					width: this.gdW,
+					float: 'left'
+				});
+
+			}else{
+				this.effect_default()
+			}
+		}
+	}
+	
+	window.tab = tab;
+})(jQuery)
+
+jQuery.fn.extend({
+	sq209_tab:function(opts){
+		return this.each(function() {
+			new tab($(this),opts)
+		});
+	}
+});
